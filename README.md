bcb9d17ad2344784b7bdcff057a235e0662df45a
gianpc <gpaucarcortez@gmail.com>
2024-11-06 14:25:41 -0500

    Agregar un boton que activa un form

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 
 1. Mostrando la lista de espectaculos
 2. Agregando un componente **Espectaculo**
+3. Uso del Hook **useState**
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-import "./App.css"
+import { useState } from "react";
+
+
 import Espectaculo from "./Components/Espectaculo";
-import {espectaculos} from "./espectaculos";
+import { espectaculos } from "./espectaculos";
 
 
 function App() {
-  
+
+
+  const [isSelected, setisSelected] = useState(false)
 
   return (
     <>
-      {espectaculos.map((elemento)=>(
-        <Espectaculo elemento={elemento} key={elemento.id}/>
+      {
+        isSelected ? <form>formulario</form> : null
+      }
+      {espectaculos.map((elemento) => (
+        <Espectaculo elemento={elemento} key={elemento.id} />
       ))}
+      <button onClick={()=>setisSelected(true)}>Quiero ir</button>
     </>
-    
+
   )
 }
 
