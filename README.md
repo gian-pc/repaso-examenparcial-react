cb275fed5de009f7fee03030d5c19ff3256f66da
gianpc <gpaucarcortez@gmail.com>
2024-11-06 15:09:30 -0500

    Implementar el formulario

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 3. Uso del Hook **useState** y agregamos un boton que active un formulario
 4. Movemos el boton a Espectaculo y añadimos un nuevo prop(setisSelected)
 5. Agregamos un nuevo componente **Form**
+6. Implementamos el **Form**
 
 
 
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
     // estados
+    const [comprador, setComprador] = useState({
+        nombre: "",
+        email: ""
+    })
+
     // validaciones
+    let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    if(comprador.nombre.length > 6 && comprador.nombre.includes(" ") && regexEmail.test(comprador.email)){
+
+    }
+
     // onSubmit
-    // onChange
-    // mostrar un error
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+    }
+
+    
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Nombre completo:</label>
-                <input type="text" />
+                // onChange
+                <input type="text" onChange={(e) => setComprador({ ...comprador, nombre: e.target.value })} />
+
                 <label>Email:</label>
-                <input type="text" />
+                // onChange
+                <input type="email" onChange={(e) => setComprador({ ...comprador, email: e.target.value })} />
                 <button>Comprar entrada</button>
             </form>
+            // mostrar un error
         </div>
+        
     )
 }
 
